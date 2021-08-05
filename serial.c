df087d13d3191acf206c6cad1da58b9bd9dd2104
Mateus Dutra <mdutras@users.noreply.github.com>
2021-08-05 07:22:54 -0300

    remoção de comentários para limpeza do código

--- serial.c
+++ serial.c
@@ -6,10 +6,8 @@
 
 void crivo(int inicio, int fim, int *n){
     int multiples[] = {2,3,5,7};
-    // #pragma omp parallel for
     for(int i = 0; i < (fim - inicio) ; i++){
         n[i] = 1;
-        // #pragma omp parallel for
         for(int j = 0; j < 4; j++){
             if(((inicio + i) != multiples[j]) & ((inicio + i) % multiples[j] == 0)){
                 n[i] = 0;
@@ -32,27 +30,7 @@ int main(){
             printf("\n");
         }
     }
-    // int num_primos = 0;
-    // printf("%d\n", size);
-    // int multiples[] = {2,3,5,7};
-    // green(1, size);
-    // for(int i = 2; i <= NUM_ELEMS ; i++){
-    //     b[i] = 1;
-    //     for(int j = 0; j < 4; j++){
-    //         if((i != multiples[j]) & (i % multiples[j] == 0)){
-    //             b[i] = 0;
-    //             num_primos++;
-    //         }
-    //     }
-    //     if(b[i]){
-    //         green(i, size);
-    //     }else{
-    //         red(i, size);
-    //     }
-    //     if(i % 10 == 0){
-    //         printf("\n");
-    //     }
-    // }
+    printf("\n");
     return 0;
 
 }
